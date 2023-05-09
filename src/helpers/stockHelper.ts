063b6b5622a01e30650c6bc312c0938c2b675e25
Ken Yun <yunhaen@gmail.com>
2023-05-08 20:29:19 -0400

    Created helper functions for individual stock page

--- src/helpers/stockHelper.ts
+++ src/helpers/stockHelper.ts
@@ -1,3 +1,24 @@
+import { toast } from "react-toastify";
+
 export function convertVolumeToShorthand(volume: number) {
   return new Intl.NumberFormat('en-US', { notation: 'compact'}).format(volume);
+}
+
+export function formatToDollar(cost: number) {
+  return new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'}).format(cost);
+}
+
+export function calculateCostOfShares(amountString: string, closePrice: number) {
+  if (amountString === '') return "$0.00";
+  let amount = parseFloat(amountString);
+  return formatToDollar(amount * closePrice);
+}
+
+export function handlePurchaseShares(amountString: string, volume: number) {
+  let amount = parseFloat(amountString);
+  if (amount > volume) {
+    toast.error("Not enough shares available!");
+    return;
+  }
+  
 }
