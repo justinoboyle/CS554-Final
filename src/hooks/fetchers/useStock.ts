bd691c754f1f4869f3bddb53f513bdda04865c83
Ken Yun <yunhaen@gmail.com>
2023-05-09 19:55:02 -0400

    Changed stock ticker endpoint to POST, receives ticker and date (optional) in body

--- src/hooks/fetchers/useStock.ts
+++ src/hooks/fetchers/useStock.ts
@@ -1,7 +1,16 @@
 // api/tools/security[id] check if security exists
 
-export async function fetchStock(ticker: string) {
-  const response = await fetch(`/api/stock/${ticker}`);
+export async function fetchStock(ticker: string, date?: Date) {
+  const response = await fetch('/api/stock', {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      ticker,
+      date,
+    })
+  });
   const data = await response.json();
   return data;
 }
